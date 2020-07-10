dc57fbc7105c67dc1c5824d4c4ba99c9b6203e38
komal.iconflux <komal.akhani@iconflux.com>
2020-07-10 13:12:19 +0530

    country dialog

--- app/src/main/java/com/semonics/tiktik/Accounts/SignUpActivity.java
+++ app/src/main/java/com/semonics/tiktik/Accounts/SignUpActivity.java
@@ -3,24 +3,35 @@ package com.semonics.tiktik.Accounts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.semonics.tiktik.Model.CountryModel;
 import com.semonics.tiktik.R;
+import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView tvLogIn;
-    EditText etBirthday;
+    private TextView tvLogIn,tvCountryName;
+    private ImageView ivCountryImg;
+    private LinearLayout llCountry;
     private DatePickerDialog mDatePickerDialog;
 
     @Override
@@ -28,37 +39,44 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         tvLogIn = findViewById(R.id.tv_log_in);
-        etBirthday = findViewById(R.id.activity_sign_up_et_birthday);
-        setDateTimeField();
+        llCountry = findViewById(R.id.activity_sign_up_ll_country);
+        tvCountryName = findViewById(R.id.activity_sign_up_tv_country_name);
+        ivCountryImg = findViewById(R.id.activity_sign_up_iv_country);
         tvLogIn.setOnClickListener(this);
-        etBirthday.setOnTouchListener(new View.OnTouchListener() {
+        llCountry.setOnClickListener(this);
+
+    }
+
+    private void showAlertDialog() {
+        // Prepare grid view
+        GridView gridView = new GridView(this);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final List<CountryModel> mList = new ArrayList<CountryModel>();
+        CountryModel countryModel = new CountryModel();
+        countryModel.setCountryName("India");
+        countryModel.setCountryImg("https://mk0businessofem29abh.kinstacdn.com/wp-content/uploads/2020/03/photo-earth.jpg");
+        mList.add(countryModel);
+
+
+        gridView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, mList));
+        gridView.setNumColumns(5);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                mDatePickerDialog.show();
-                return false;
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // do something here
+                tvCountryName.setText(mList.get(position).getCountryName());
+                Picasso.with(SignUpActivity.this).load(mList.get(position).getCountryImg()).resize(400,400).centerCrop().into(ivCountryImg);
+
             }
         });
 
+        // Set grid view to alertDialog
+
+        builder.setView(gridView);
+        builder.setTitle("Select Country");
+        builder.show();
     }
-    private void setDateTimeField() {
 
-        Calendar newCalendar = Calendar.getInstance();
-        mDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                Calendar newDate = Calendar.getInstance();
-                newDate.set(year, monthOfYear, dayOfMonth);
-                SimpleDateFormat sd = new SimpleDateFormat("dd-MM-yyyy");
-                final Date startDate = newDate.getTime();
-                String fdate = sd.format(startDate);
-
-                etBirthday.setText(fdate);
-
-            }
-        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
-        mDatePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-
-    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -72,7 +90,9 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                 overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
                 finish();
                 break;
-
+            case R.id.activity_sign_up_ll_country:
+                showAlertDialog();
+                break;
         }
     }
 }
---
+++ app/src/main/java/com/semonics/tiktik/Model/CountryModel.java
@@ -0,0 +1,23 @@
+package com.semonics.tiktik.Model;
+
+public class CountryModel {
+    String countryImg;
+    String countryName;
+
+    public String getCountryImg() {
+        return countryImg;
+    }
+
+    public void setCountryImg(String countryImg) {
+        this.countryImg = countryImg;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
+
